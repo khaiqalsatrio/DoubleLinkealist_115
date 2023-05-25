1855fe7b17bc827e7683d843cce625eac77cfb95
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-05-25 20:27:45 +0700

    void hapus

--- DoubleLinkealist_115/DoubleLinkealist_115.cpp
+++ DoubleLinkealist_115/DoubleLinkealist_115.cpp
@@ -137,3 +137,18 @@ void DoubleLinkedList::revtraverse() {
 		}
 	}
 }
+
+void DoubleLinkedList::hapus() {
+	if (listEmpty()) {
+		cout << "\nList is Empty" << endl;
+
+	}
+	cout << "\nEnter the roll number of the student whose record is to be delete: ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "record not found" << endl;
+	else
+		cout << "record with roll number" << rollNo << "delete" << endl;
+}
